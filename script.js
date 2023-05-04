77e2f3e791b85694ccb5448765b6472ddeae114a
NathanAzvdo <nathan.azevedo28@gmail.com>
2023-05-04 20:42:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let cont =0;
+
+function calc(){
+    let lucro = document.getElementById('lucro').value;
+    let custo = document.getElementById('custo').value;
+    if(lucro!=="" && custo!==""){
+
+        valorF = parseFloat(custo)+((parseFloat(custo)/100)*parseFloat(lucro));
+        let venda = document.getElementById('venda');
+        
+        venda.value = "R$"+valorF.toFixed(2);
+    }
+    else if(custo==""){
+        venda.value = "";
+    }
+    else if(lucro=="" && custo!==""){
+        venda.value = "R$"+custo;
+    }
+}
+
+
+
+function color(){
+    var body = document.body;
+    var fiel = document.getElementById('field');
+    var leg = document.getElementById('leg');
+    var sub = document.getElementById('submit');
+    var iUser = document.getElementsByClassName("inputUser");
+    var iLabel = document.getElementsByClassName("labelInput");
+    var btnColor = document.getElementById("btnColor");
+    body.classList.toggle("bodyDark");
+    fiel.classList.toggle("fieldsetDark");
+    leg.classList.toggle("legendDark");
+    sub.classList.toggle("submitDark");
+    for(var i = 0; i<iUser.length; i++){
+        iUser[i].classList.toggle("inputUserDark");
+    }
+    for(var i = 0; i<iLabel.length; i++){
+        iLabel[i].classList.toggle("labelInputDark");
+    }
+    cont++;
+    if(cont%2==1){
+        btnColor.innerText="Modo Claro"
+    }
+    else{
+        btnColor.innerText="Modo Escuro"
+    }
+  
+
+    
+}
